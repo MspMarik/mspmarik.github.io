82bf7fdc3add2947a6705aa52fea4bc98242148f
Mark Spivak <msp3472@gmail.com>
2022-09-23 13:17:49 -0400

    made proj images zoom with btn toggle

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ async function removeElems() {
     document.getElementById("loadingText").remove();
     document.getElementById("defaultCanvas0").remove();
 }
+
+function toggleZoom(pics, btn) {
+    let childs = pics.children;
+    if (childs[0].style.width == "100%") {
+        btn.innerHTML = "Click here to enlarge image(s)";
+        for (let child of childs) {
+            child.style.width = "45%";
+        }
+    } else {
+        btn.innerHTML = "Click here to shrink image(s)";
+        for (let child of childs) {
+            child.style.width = "100%";
+        }
+    }
+}
