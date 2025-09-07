c34f62d58f4e5a042d70a6ebb1d1a7ee6780cd6d
Mark Spivak <msp3472@gmail.com>
2025-09-07 00:33:22 -0400

    added work timeline and some cleanup

--- work.js
+++ work.js
@@ -1,8 +1,19 @@
 function normalizeTimelineWidths() {
     let timeline_items = $("#timeline-list").children();
-    for (let i = 1; (i = timeline_items.length); i++) {
-        timeline_items[i].width(timeline_items[i - 1]);
+    // Adjust loop to avoid array out of bounds (length-1)
+    for (let i = 1; i < timeline_items.length; i++) {
+        // Use proper jQuery methods for width setting
+        $(timeline_items[i]).width($(timeline_items[i - 1]).width());
     }
 }
 
+// Initial call to normalize widths
 normalizeTimelineWidths();
+
+// Replace resize event with ResizeObserver for better element size monitoring
+const resizeObserver = new ResizeObserver(() => {
+    normalizeTimelineWidths();
+});
+
+// Start observing the first timeline item
+resizeObserver.observe($("#timeline-list").children().first()[0]);
